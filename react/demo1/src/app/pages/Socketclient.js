856a32c8f8a9dc674e30d5254ae26b2d3def5dff
pooja2000pm <pooja2000pm@gmail.com>
2022-06-05 23:14:28 +0530

    initial commit

--- react/demo1/src/app/pages/Socketclient.js
+++ react/demo1/src/app/pages/Socketclient.js
@@ -1,19 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Button } from "react-bootstrap-v5";
 import  io  from "socket.io-client";
-const ENDPOINT = "http://127.0.0.1:8081";
 
-const socket = io.connect(ENDPOINT);
+const socket = io.connect("http://localhost:3001");
 
 function Socketclient() {
     const [message, setMessage] = useState("");
-    const [messageReceived, setMessageReceived] = useState("");
+   const [messageReceived, setMessageReceived] = useState("");
     const sendMessage = () => {
-      socket.emit("send_message", { message  });
+      socket.emit("send_message", { message });
     }
   useEffect(() => {
    
-    socket.on("receive-message", (data) => {setMessageReceived(data.message)});
+    socket.on("receive_message", (data) => {setMessageReceived(data.message)});
   }, []);
 
   return (   
@@ -22,18 +21,24 @@ function Socketclient() {
 <div className="container my-3">
     <div className="row">
       <section>
-   <form method="POST" name= "form2" > 
+   
 <label>Hostname</label>
-<input type="text" className ="form-control " placeholder=" Enter hostname / ip address" id = "hostname" name = "host" onChange={(event) => {
-  setMessage(event.target.value)
-}} />
+<input type="text" className ="form-control " placeholder=" Enter hostname / ip address" id = "hostname" name = "host" onChange={(event) => { 
+  setMessage(event.target.value);
+
+}} 
+ />
  <Button className ="btn btn-primary btn-hover-rise me-4" type="submit " onClick={sendMessage()} >Scan </Button>
  <p>
       Web socket connection testing! 
       Message from server!!
-      {messageReceived}
+      
+      
+    
      </p>
- </form>
+     <br>
+     </br>
+     {messageReceived}
  </section>
  </div>
 </div>
